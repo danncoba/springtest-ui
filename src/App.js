daf82d7fa40cd8d98f7ba798ef484d98ad4a202d
Danijel Djuric <ddjuric@apple.com>
2023-03-03 13:37:38 +0100

    Added new change

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello To All and welcome again</h1>
+        <h2>Another way in</h2>
       </header>  
     </div>
   );
