9fbba5e7b79bd0b39ca62f430027a21b8b515645
Danijel Djuric <ddjuric@apple.com>
2023-03-03 13:34:39 +0100

    Added new value

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello To All</h1>
+        <h1>Hello To All and welcome again</h1>
       </header>  
     </div>
   );
